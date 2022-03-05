e254fd047cd45df7f40262e34506875800968746
Taabish Shaikh <taabishshaikh2@gmail.com>
2022-03-05 18:27:17 +0530

    leads added to database

--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Leads(models.Model):
+
+    def __str__(self):
+        return self.lead_name
+
+    lead_company = models.CharField(max_length=200)
+    lead_name = models.CharField(max_length=200)
+    phone_num = models.CharField(max_length=10)
+    lead_email = models.EmailField()
+    address = models.CharField(max_length=500)
+    requirement = models.CharField(max_length=1500)
+    lead_managed = models.CharField(max_length=200)
